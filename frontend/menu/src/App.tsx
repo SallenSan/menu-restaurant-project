b65d0f322178e8e6b5b845230dbcebc69a010fba
Sallenkon S. Almeida <sallenkon54@gmail.com>
2023-12-29 10:51:11 -0300

    commit5

--- frontend/menu/src/App.tsx
+++ frontend/menu/src/App.tsx
@@ -3,16 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
 
   return (
 
     <div className='Container'>
       <h1>Menu</h1>
       <div className='card-grid'>
-        {data.map(foodData =>
+        {data?.map(foodData =>
           <Card
             price={foodData.price}
             title={foodData.title}
--- frontend/menu/src/main.tsx
+++ frontend/menu/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
   </React.StrictMode>,
 )
